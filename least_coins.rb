bce4868809b011c22542ad26263800717d878faf
cassidytrinh <cassidytrinh@gmail.com>
2018-06-20 19:01:40 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,15 @@
-#write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+  money = cents
+  coins = Hash.new
+  legend = [[:quarters, 25], [:dimes, 10], [:nickels, 5], [:pennies, 1]]
+  legend.each do |coin|
+    num = money/coin[1]
+    coins[coin[0]] = num
+    money -= coin[1]*num
+  end
+  return coins
 end
+
+puts least_coins(29)
+puts least_coins(99)
